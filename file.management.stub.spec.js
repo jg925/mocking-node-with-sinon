3b58e98c0105377fa0c6f249219b6be9bf8eabd0
jg925 <jg925@cornell.edu>
2022-03-02 21:59:27 -0500

    stubs and fakes

--- file.management.stub.spec.js
+++ file.management.stub.spec.js
@@ -3,7 +3,7 @@ const sinon = require("sinon");
 const fs = require("fs");
 const proxyquire = require("proxyquire");
 
-describe("File Management", () => {
+describe.skip("File Management", () => {
   afterEach(() => {
     sinon.restore();
   });
@@ -23,5 +23,39 @@ describe("File Management", () => {
     const fileManagement = proxyquire("./file.management", { fs });
 
     expect(() => fileManagement.createFile("test.txt")).to.throw();
+  });
+
+  it("createFileSafe should create a file named test1 when test already exists", () => {
+    const writeStub = sinon.stub(fs, "writeFileSync");
+    const readStub = sinon.stub(fs, "readdirSync");
+
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    writeStub.withArgs("./data/test.txt").throws(new Error());
+    writeStub.returns(undefined);
+    readStub.returns(["test.txt"]);
+  });
+
+  it("getAllFiles should return a list of files", () => {
+    const readStub = sinon.stub(fs, "readdir");
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    readStub.yields(null, ["test.txt"]);
+    fileManagement.getAllFiles((err, data) => {
+      expect(data).to.eql(["test.txt"]);
+    });
+  });
+
+  it("getAllFilesPromise should return a list of files", () => {
+    const readStub = sinon.stub(fs, "readdir");
+
+    const util = {
+      promisify: sinon.stub().returns(readStub),
+    };
+    const fileManagement = proxyquire("./file.management", { fs, util });
+    readStub.resolves(["test.txt"]);
+    return fileManagement
+      .getAllFilesPromise()
+      .then((files) => expect(files).to.eql(["test.txt"]));
   });
 });
